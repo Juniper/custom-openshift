c03254e5978753159f764ace1246f9ed39ecd9b9
root <root@ip-10-10-10-15.us-west-1.compute.internal>
2018-12-10 07:45:21 +0000

    fix

--- ose/files/master_update.sh
+++ ose/files/master_update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 dest=$1
-yes | cp -rf $PWD/../files/master_main.yml $dest/roles/contrail_master/defaults/main.yml
+yes | cp -rf $PWD/../files/master_main.yaml $dest/roles/openshift_master/defaults/main.yml
 
 
