559b72121b625e26cfa5a27300639f692e654488
root <root@ip-10-10-10-15.us-west-1.compute.internal>
2018-12-11 03:45:32 +0000

    fix append code

--- ose/files/append_nodes.sh
+++ ose/files/append_nodes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 dest=$1
-yes | cp -rf  $PWD/../files/additional_config.yml >> $dest/playbooks/openshift-node/private/additional_config.yml
+yes | cp -rf  $PWD/../files/additional_config.yml $dest/playbooks/openshift-node/private/additional_config.yml
 
 
